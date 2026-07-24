3573d9ffa1c2d4c96e4243d99a0a624827c2bfe6
Mahruf Hossain <88084890+mahrufhossain@users.noreply.github.com>
2026-07-24 15:04:28 +0600

    Completed the main function.
    The demo calculator project is somewhat finished without the division feature, and lots of edge cases.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #this is the main python folder
+from add import addition
+from multiply import mult
+from sub import sub
+
+
+def main():
+    num1 = int(input("Enter the number:"))
+    num2 = int(input("Enter the second number:"))
+
+    results = [addition(num1, num2), sub(num1, num2), mult(num1, num2)]
+
+    for i in results:
+        print(i)
+
+if __name__ == "__main__":
+    main()
